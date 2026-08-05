81b4a97a9bfeff543b49b48e2444a4f4d2f98cc6
Steve-Yang-KR <73861054+Steve-Yang-KR@users.noreply.github.com>
2026-08-06 06:53:11 +0900

    Add 3D Hologram launcher to Panorama workspace

--- hologram_app.py
+++ hologram_app.py
@@ -1,7 +1,29 @@
 from fastapi import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
 
 from app import app, templates
+
+
+@app.middleware('http')
+async def add_hologram_link_to_panorama(request: Request, call_next):
+    response = await call_next(request)
+    if request.url.path != '/' or response.headers.get('content-type', '').split(';')[0] != 'text/html':
+        return response
+
+    body = b''.join([chunk async for chunk in response.body_iterator])
+    html = body.decode('utf-8')
+    script = '<script src="/static/hologram-link.js"></script>'
+    if script not in html:
+        html = html.replace('</body>', f'{script}</body>')
+
+    headers = dict(response.headers)
+    headers.pop('content-length', None)
+    return Response(
+        content=html,
+        status_code=response.status_code,
+        headers=headers,
+        media_type='text/html',
+    )
 
 
 @app.get('/hologram', response_class=HTMLResponse)
