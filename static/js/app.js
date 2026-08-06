e9cc10ef624edaddf4290f608b459b82b4dd4ca6
Steve-Yang-KR <73861054+Steve-Yang-KR@users.noreply.github.com>
2026-08-06 14:08:17 +0900

    Load merged homepage v2 component styles

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,10 @@ import {initPanorama} from './panorama.js';
 import {initTwin} from './twin.js';
 import {initSimulation} from './simulation.js';
 
+const mergedStyle=document.createElement('link');
+mergedStyle.rel='stylesheet';
+mergedStyle.href='/static/css/homepage-v2-merge.css';
+document.head.appendChild(mergedStyle);
+
 const initializers=[initNavigation,initContent,initCameras,initPanorama,initTwin,initSimulation];
 for(const init of initializers){try{init();}catch(error){console.error(`[${init.name}]`,error);}}
